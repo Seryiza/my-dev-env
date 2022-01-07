e5f4b00d58fedc074c4e0400dea5ac0a80e4f1bd
Sergey Zaborovsky <git@seryiza.xyz>
2022-01-07 21:07:59 +1100

    Add support to run playbook with passwords from ansible-vault

--- sync-dev-env.sh
+++ sync-dev-env.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+cd `dirname "$0"`
+
 # Install Ansible if you don't have it
 if !command -v ansible &> /dev/null
 then
@@ -9,5 +11,12 @@ then
 	sudo apt install -y ansible
 fi
 
-# Run playbook
-ansible-playbook -i hosts setup-dev-env.yml
+if [ -f ./passwords.yml ] && [ -f ./vault-password ]
+then
+    # Run playbook with passwords from vault
+    ansible-playbook -i hosts -e @passwords.yml --vault-password-file vault-password dev-env.yml
+else
+    # Run playbook with password from prompt
+    ansible-playbook -i hosts --ask-become-pass dev-env.yml
+fi
+
