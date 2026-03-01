590e1f9a0e9a5e2750146a2783c4c76122e373c6
Sergey Zaborovsky <git@seryiza.xyz>
2026-03-02 02:10:33 +0600

    nixos: fix "mu init" when it iss running in emacs

--- nixos/home/email.nix
+++ nixos/home/email.nix
@@ -1,15 +1,57 @@
-{ config, pkgs, lib, ... }:
+{ config, lib, pkgs, ... }:
 let
+  inherit (lib) concatStringsSep filter flatten;
+
   maildir = "${config.home.homeDirectory}/Maildir";
-  offlineimapBin = lib.getExe pkgs.offlineimap;
+
+  muAccounts = filter (a: a.enable && a.mu.enable)
+    (lib.attrValues config.accounts.email.accounts);
+
+  addresses = map (a: a.address) muAccounts
+    ++ map (alias: alias.address or alias)
+    (flatten (map (a: a.aliases or [ ]) muAccounts));
+
+  addressesShell = concatStringsSep " " (map lib.escapeShellArg addresses);
+
+  myAddresses =
+    concatStringsSep " " (map (addr: "--my-address=" + addr) addresses);
+
+  bins = {
+    offlineimap = lib.getExe pkgs.offlineimap;
+    mu = lib.getExe pkgs.mu;
+    gawk = lib.getExe pkgs.gawk;
+    grep = lib.getExe pkgs.gnugrep;
+    pgrep = lib.getExe' pkgs.procps "pgrep";
+  };
 in {
-  home.packages = [
-    pkgs.offlineimap
-    pkgs.mu
-  ];
+  home.packages = with pkgs; [ offlineimap mu ];
 
   programs.offlineimap.enable = true;
   programs.mu.enable = true;
+
+  home.activation.runMuInit = lib.mkForce
+    (lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      if ${bins.pgrep} -f "mu server" >/dev/null 2>&1; then
+        echo "runMuInit: mu server running; skipping init" >&2
+        exit 0
+      fi
+
+      MUHOME="${config.programs.mu.home}"
+      MU_ADDRS=$(( \
+        ${bins.mu} info store 2>/dev/null | \
+        ${bins.gawk} '/personal-address/{print $4}' | \
+        paste -sd ' ' \
+      ) || true)
+      missing=0
+      for addr in ${addressesShell}; do
+        echo "$MU_ADDRS" | ${bins.grep} -Fqx "$addr" || missing=1
+      done
+
+      if [[ ! -d "$MUHOME" || "$missing" -eq 1 ]]; then
+        run ${bins.mu} init --maildir=${maildir} --muhome "$MUHOME" \
+          ${myAddresses} $VERBOSE_ARG
+      fi
+    '');
 
   accounts.email = {
     maildirBasePath = maildir;
@@ -36,7 +78,7 @@ in {
     Unit.Description = "OfflineIMAP sync";
     Service = {
       Type = "oneshot";
-      ExecStart = offlineimapBin;
+      ExecStart = bins.offlineimap;
       Environment =
         "PATH=${lib.makeBinPath [ pkgs.coreutils pkgs.mu pkgs.offlineimap ]}";
     };
